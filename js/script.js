7ef8143729b8b544c7edba618ea7cf1212c0cabe
aiste_miskinis <aiste.miskinis@gmail.com>
2025-12-10 14:31:15 -0600

    updated script.js

--- js/script.js
+++ js/script.js
@@ -77,14 +77,15 @@ const displayRepoInfo = function (repoInfo, languages) {
     repoData.innerHTML = "";
     repoData.classList.remove("hide");
     allReposContainer.classList.add ("hide");
+
     const div = document.createElement("div");
     div.innerHTML = `
     <h3>Name: ${repoInfo.name}</h3>
     <p>Description: ${repoInfo.description}</p>
     <p>Default Branch: ${repoInfo.default_branch}</p>
     <p>Languages: ${languages.join(", ")}</p>
-    <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
-`;
+    <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub! </a>`;
+    
 repoData.append(div);
 };
 
